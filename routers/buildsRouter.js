2051a60791081413637f9770092c70a133708770
Didrik <didrik.andersson@mio.se>
2022-10-10 19:34:34 +0200

    add post enpoint for builds model

--- routers/buildsRouter.js
+++ routers/buildsRouter.js
@@ -4,12 +4,29 @@ const Build = require("../models/buildModel");
 
 router.get("/", async (req, res) => {
     try {
-      const builds = await Build.find();
-      res.json(builds);
+        const builds = await Build.find();
+        res.json(builds);
     } catch (err) {
-      console.error(err);
-      res.status(500).send();
+        console.error(err);
+        res.status(500).send();
     }
-  });
+});
 
-  module.exports = router;
+router.post("/", async (req, res) => {
+    try {
+        const { name } = req.body;
+
+        const newBuild = new Build({
+            name,
+        });
+
+        const savedBuild = await newBuild.save();
+
+        res.json(savedBuild);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+module.exports = router;
